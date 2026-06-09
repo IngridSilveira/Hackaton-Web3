fff190428ebb59dc26824389a41add2dd39573b4
Edmar-Sousa <edmarsousamendes4@gmail.com>
2026-06-09 01:17:09 -0300

    feat: description to modal

--- frontend/src/components/modalWithdraw.tsx
+++ frontend/src/components/modalWithdraw.tsx
@@ -1,4 +1,6 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 interface ModalWithdrawProps {
     onClose: (visible: boolean) => void
@@ -11,7 +13,20 @@ export function ModalWithdraw(props: ModalWithdrawProps) {
     return (
         <div className="fixed px-4 left-0 top-0 right-0 bottom-0 bg-black/30 flex items-center justify-center">
             <div className="bg-white w-full max-w-150 p-4 rounded-2xl">
-                <h2 className="text-xl font-bold">Solicitação de Saque</h2>
+                <h2 className="text-xl font-bold">
+                    Solicitação de Saque
+                </h2>
+
+                <p className="mt-4 text-[14px]">
+                    Para fazer um saque, você precisa fazer um upload de um arquivo 
+                    que comprove vericidade! Depois de uma analise e votação dos doadores 
+                    os recursos seram liberados.
+                </p>
+
+                <form className="mt-5 mb-10">
+                    <Label htmlFor="file-input">Arquivo:</Label>
+                    <Input type="file" id="file-input" className="mt-3" />
+                </form>
 
                 <div className="flex gap-4">
                     <Button onClick={() => onClose(false)} className="flex-1 py-6 cursor-pointer" variant="outline">
